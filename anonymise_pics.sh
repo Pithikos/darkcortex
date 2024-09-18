1795b3bfa42f1041f058b6f579333279b51e9354
Manos <manossef@gmail.com>
2024-09-18 19:26:05 +0100

    Fix uppercase JPG

--- anonymise_pics.sh
+++ anonymise_pics.sh
@@ -2,7 +2,7 @@
 
 # 1. Anonymise all pictures in /uploads/*.png|jpg to use the md5 hash of the filename as the new filename
 # 2. At the same time replace all index.html with pattern "/uploads/<filename>"
-for file in ./uploads/*.{png,jpg}; do
+for file in ./uploads/*.{png,jpg,JPG}; do
     new_basename=$(md5sum "$file" | cut -d ' ' -f 1)
     file_extension="${file##*.}"
     new_filename="$new_basename.$file_extension"
